cd8583a0de4c7ba05d7d5324e5c638f2f1576800
Hoyoung Lee <50818662+sleepbysleep@users.noreply.github.com>
2022-12-16 13:13:45 +0900

    Add the variation for Pyside6

---
+++ pyside6_range_slider/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys, os
+from PySide6 import QtCore, QtGui, QtWidgets
+import range_slider
+
+def echo(low_value, high_value):
+	print(low_value, high_value)
+
+def main(argv):
+    app = QtWidgets.QApplication(sys.argv)
+    slider = range_slider.RangeSlider(QtCore.Qt.Horizontal)
+    slider.setMinimumHeight(30)
+    slider.setMinimum(0)
+    slider.setMaximum(255)
+    slider.setLow(15)
+    slider.setHigh(35)
+    slider.setTickPosition(QtWidgets.QSlider.TicksBelow)
+    slider.sliderMoved.connect(echo)
+    #QtCore.QObject.connect(slider, QtCore.SIGNAL('sliderMoved(int)'), echo)
+    slider.show()
+    slider.raise_()
+    app.exec()
+
+if __name__ == "__main__":
+    main(sys.argv)
+
